454d172b48b496167b991865d68f2f9273ec70df
Dipak Sarkar <hello@dipaksarkar.in>
2024-05-03 20:51:10 +0530

    Updated Image Component

--- ui/src/components/plugins/index.js
+++ ui/src/components/plugins/index.js
@@ -1,8 +1,11 @@
 import loadBlocks from './blocks'
 import loadShortCodes from './shortcodes'
+import updateDefault from './update-default'
 
 export default (editor, opt = {}) => {
   const options = { ...opt, category: 'Basic' }
+
+  updateDefault(editor, options)
 
   loadShortCodes(editor, options)
   loadBlocks(editor, options)
