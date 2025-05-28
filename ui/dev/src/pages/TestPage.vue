5ec83f5f2db01a32e3a799e67e4923e041f4e8a3
Dipak Sarkar <hello@dipaksarkar.in>
2025-05-28 16:36:59 +0530

    [Refactor] Remove unused data loading and storing functions

--- ui/dev/src/pages/TestPage.vue
+++ ui/dev/src/pages/TestPage.vue
@@ -121,34 +121,6 @@ const onRenameDone = () => {
   rename.value = {}
 }
 
-const loadData = (id) => {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`http://localhost:3000/projects/${id}`)
-      .then(({ data }) => {
-        resolve(data.data)
-      })
-      .catch((error) => {
-        console.error(error)
-        reject(error)
-      })
-  })
-}
-
-const storeData = (id, data) => {
-  return new Promise((resolve, reject) => {
-    axios
-      .put(`http://localhost:3000/projects/${id}`, data)
-      .then(({ data }) => {
-        resolve(data.data)
-      })
-      .catch((error) => {
-        console.error(error)
-        reject(error)
-      })
-  })
-}
-
 const config = {
   height: 'calc(100vh - 50px)',
   layerManager: {
